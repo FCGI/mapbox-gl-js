b1672792d4624d161dde5d471bf6757f71fc90e9
Shane98c <shane98c@GMAIL.COM>
2019-11-01 13:39:06 -0500

    local sprites working

--- src/style/load_sprite.js
+++ src/style/load_sprite.js
@@ -1,23 +1,31 @@
 // @flow
 
-import { getJSON, getImage, ResourceType } from '../util/ajax';
+import { getJSON, getImage, ResourceType } from "../util/ajax";
 
-import browser from '../util/browser';
-import { normalizeSpriteURL } from '../util/mapbox';
-import { RGBAImage } from '../util/image';
+import browser from "../util/browser";
+import { normalizeSpriteURL, isMapboxURL } from "../util/mapbox";
+import { RGBAImage } from "../util/image";
 
-import type {StyleImage} from './style_image';
-import type {RequestTransformFunction} from '../ui/map';
-import type {Callback} from '../types/callback';
-import type {Cancelable} from '../types/cancelable';
+import type { StyleImage } from "./style_image";
+import type { RequestTransformFunction } from "../ui/map";
+import type { Callback } from "../types/callback";
+import type { Cancelable } from "../types/cancelable";
 
-export default function(baseURL: string,
-                          transformRequestCallback: RequestTransformFunction,
-                          callback: Callback<{[string]: StyleImage}>): Cancelable {
+export default function(
+    baseURL: string,
+    transformRequestCallback: RequestTransformFunction,
+    callback: Callback<{ [string]: StyleImage }>
+): Cancelable {
     let json: any, image, error;
-    const format = browser.devicePixelRatio > 1 ? '@2x' : '';
-
-    let jsonRequest = getJSON(transformRequestCallback(normalizeSpriteURL(baseURL, format, '.json'), ResourceType.SpriteJSON), (err, data) => {
+    const format = browser.devicePixelRatio > 1 ? "@2x" : "";
+    console.log(baseURL);
+    let spriteJSONUrl = isMapboxURL(baseURL)
+        ? transformRequestCallback(
+              normalizeSpriteURL(baseURL, format, ".json"),
+              ResourceType.SpriteJSON
+          )
+        : { url: `${baseURL + format}.json` };
+    let jsonRequest = getJSON(spriteJSONUrl, (err, data) => {
         jsonRequest = null;
         if (!error) {
             error = err;
@@ -25,8 +33,13 @@ export default function(baseURL: string,
             maybeComplete();
         }
     });
-
-    let imageRequest = getImage(transformRequestCallback(normalizeSpriteURL(baseURL, format, '.png'), ResourceType.SpriteImage), (err, img) => {
+    let spritePNGUrl = isMapboxURL(baseURL)
+        ? transformRequestCallback(
+              normalizeSpriteURL(baseURL, format, ".png"),
+              ResourceType.SpriteImage
+          )
+        : { url: `${baseURL + format}.png` };
+    let imageRequest = getImage(spritePNGUrl, (err, img) => {
         imageRequest = null;
         if (!error) {
             error = err;
@@ -43,10 +56,16 @@ export default function(baseURL: string,
             const result = {};
 
             for (const id in json) {
-                const {width, height, x, y, sdf, pixelRatio} = json[id];
-                const data = new RGBAImage({width, height});
-                RGBAImage.copy(imageData, data, {x, y}, {x: 0, y: 0}, {width, height});
-                result[id] = {data, pixelRatio, sdf};
+                const { width, height, x, y, sdf, pixelRatio } = json[id];
+                const data = new RGBAImage({ width, height });
+                RGBAImage.copy(
+                    imageData,
+                    data,
+                    { x, y },
+                    { x: 0, y: 0 },
+                    { width, height }
+                );
+                result[id] = { data, pixelRatio, sdf };
             }
 
             callback(null, result);
